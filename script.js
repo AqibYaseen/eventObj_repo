f4b1808b5ccea926af0ac0c21a3461220440ad6b
AQIBYASEENBHAT <AQIBYASEENBHAT@GMAIL.COM>
2022-06-14 12:58:22 +0530

    final commit

--- script.js
+++ script.js
@@ -4,8 +4,8 @@ document.getElementById("container").addEventListener("mousemove",function(e){
     dxCor = e.clientX;
     dyCor = e.clientY;
 
-    document.getElementById("xCord").innerHTML = "Mouse X Cordinates" +e.clientX +"<br/>";
-    document.getElementById("yCord").innerHTML = "Mouse Y Cordinates" +e.clientY +"<br/>";
+    document.getElementById("xCord").innerHTML = "Mouse X Cordinates : " +e.clientX ;
+    document.getElementById("yCord").innerHTML = "Mouse Y Cordinates : " +e.clientY ;
 
 
 });
@@ -13,18 +13,17 @@ document.getElementById("container").addEventListener("mousemove",function(e){
 document.getElementById("img1").addEventListener("mousemove", (e)=>{
         ixCor = e.clientX;
         iyCor = e.clientY;
-        document.getElementById("ixCord").innerHTML = "Img X Cordinates" +e.clientX +"<br/>";
-        document.getElementById("iyCord").innerHTML = "Img Y Cordinates" +e.clientY +"<br/>";
+        document.getElementById("ixCord").innerHTML = "Img X Cordinates : " +e.clientX ;
+        document.getElementById("iyCord").innerHTML = "Img Y Cordinates : " +e.clientY ;
     
         if(dxCor==ixCor&&dyCor==iyCor){
             document.getElementById("img1").style.display= "none";
-            document.getElementById("container").innerHTML +='<img src = "https://picsum.photos/200/300">';
+            // document.getElementById("container").innerHTML +='<img src = "https://picsum.photos/200/300">';
             
         }
 })
 document.getElementById("btn").addEventListener("click",function(e){
-    console.log(e.clientX);
-
-    // e.target.client= "byy";
+    
+        document.location.reload();
 });
 
